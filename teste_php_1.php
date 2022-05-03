badcf1a214a1368f7f3597444ee55e40b3812b68
js-anderson <103535007+js-anderson@users.noreply.github.com>
2022-05-03 15:43:31 -0300

    Update teste_php_1.php

--- teste_php_1.php
+++ teste_php_1.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title></title>
 
-</head>
-
-<body>
 
 	<?php
 		function SeculoAno($ano) 
@@ -30,5 +21,3 @@
 
 
 
-</body>
-</html>
